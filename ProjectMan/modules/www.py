bcef5a6f9002d28ce6bf19d114496946a87dd5fb
Xyanzdom <128714370+Xyanzdom@users.noreply.github.com>
2023-04-10 19:04:04 +0700

    Update www.py

--- ProjectMan/modules/www.py
+++ ProjectMan/modules/www.py
@@ -69,20 +69,13 @@ async def nearest_dc(client: Client, message: Message):
 async def pingme(client: Client, message: Message):
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    xx = await edit_or_reply(message, "**0% ▒▒▒▒▒▒▒▒▒▒**")
-    await xx.edit("**20% ██▒▒▒▒▒▒▒▒**")
-    await xx.edit("**40% ████▒▒▒▒▒▒**")
-    await xx.edit("**60% ██████▒▒▒▒**")
-    await xx.edit("**80% ████████▒▒**")
-    await xx.edit("**100% ██████████**")
-    end = datetime.now()
-    duration = (end - start).microseconds / 1000
+
+
     await xx.edit(
-        f"❏ **PONG!!🐣**\n"
+        f"❏ **PONG✈️**\n"
         f"├• **Pinger** - `%sms`\n"
-        f"├• **Uptime -** `{uptime}` \n"
-        f"└• **Owner :** {client.me.mention}" % (duration)
-    )
+
+       
 
 
 @Client.on_message(filters.command("kping", cmd) & filters.me)
